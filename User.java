0cbafc6a635bb3a2a88be68a32ad5d35dbb5c730
julius <ghoussysh@gmail.com>
2019-06-14 20:40:43 +0200

    third intial

--- User.java
+++ User.java
@@ -8,6 +8,11 @@ Rank a = Rank.Major;
  switch(a){
  	case Major:
  		System.out.println("the Major says fire");
+ 		break;
  	case General:
  		System.out.println("hold your fire Captain");
+ 		break;
+ 	case Captain:
+ 		System.out.println("jowel herr general");
+ 		break;
  }
